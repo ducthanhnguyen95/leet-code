297e6cb8923f8a09f9879e0ec86196f6d40c6905
thanhnd <ducthanhnguyen95@gmail.com>
2025-01-07 23:40:01 +0700

    Two sum

---
+++ two_sum/two_sum.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(twoSum([]int{2, 7, 11, 15}, 9))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+	fmt.Println(twoSum([]int{3, 3}, 6))
+}
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[j] == target-nums[i] {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{}
+}

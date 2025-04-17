5dc2ea4d6e2e45931f8ecf0268b4be3fe9dbac70
thanhnd <ducthanhnguyen95@gmail.com>
2025-04-17 21:07:41 +0700

    Update two sum

--- easy/1_two_sum/1_two_sum.go
+++ easy/1_two_sum/1_two_sum.go
@@ -7,13 +7,27 @@ func main() {
 	fmt.Println(twoSum([]int{3, 2, 4}, 6))
 	fmt.Println(twoSum([]int{3, 3}, 6))
 }
+
+//func twoSum(nums []int, target int) []int {
+//	for i := 0; i < len(nums); i++ {
+//		for j := i + 1; j < len(nums); j++ {
+//			if nums[j] == target-nums[i] {
+//				return []int{i, j}
+//			}
+//		}
+//	}
+//	return []int{}
+//}
+
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums); i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[j] == target-nums[i] {
-				return []int{i, j}
-			}
+	numIndexes := make(map[int]int) // num -> index
+
+	for i, num := range nums {
+		diff := target - num
+		if idx, ok := numIndexes[diff]; ok {
+			return []int{i, idx}
 		}
+		numIndexes[num] = i
 	}
-	return []int{}
+	return nil
 }

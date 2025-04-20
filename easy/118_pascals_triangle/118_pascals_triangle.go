93639e148c775d22ad167ec8c41cfb0da524cabe
thanhnd <ducthanhnguyen95@gmail.com>
2025-04-20 21:04:36 +0700

    Pascal's Triangle

--- easy/118_pascals_triangle/118_pascals_triangle.go
+++ easy/118_pascals_triangle/118_pascals_triangle.go
@@ -7,21 +7,40 @@ func main() {
 
 }
 
-func generate(numRows int) [][]int {
-	var output [][]int
-	for i := 0; i < numRows; i++ {
-		var num []int
-		for j := 0; j <= i; j++ {
-			if j == 0 || j == i {
-				num = append(num, 1)
-			} else {
-				left := output[i-1][j-1]
-				right := output[i-1][j]
-				num = append(num, left+right)
-			}
-		}
-		output = append(output, num)
-	}
-	return output
+//func generate(numRows int) [][]int {
+//	var output [][]int
+//	for i := 0; i < numRows; i++ {
+//		var num []int
+//		for j := 0; j <= i; j++ {
+//			if j == 0 || j == i {
+//				num = append(num, 1)
+//			} else {
+//				left := output[i-1][j-1]
+//				right := output[i-1][j]
+//				num = append(num, left+right)
+//			}
+//		}
+//		output = append(output, num)
+//	}
+//	return output
+//
+//}
 
+func generate(numRows int) [][]int {
+	result := [][]int{}
+	arr := []int{1}
+	result = append(result, arr)
+
+	for i := 1; i < numRows; i++ {
+		prevRow := result[len(result)-1]
+		currRow := []int{1}
+
+		for j := 1; j < len(prevRow); j++ {
+			currRow = append(currRow, prevRow[j-1]+prevRow[j])
+		}
+		currRow = append(currRow, 1)
+		result = append(result, currRow)
+	}
+
+	return result
 }

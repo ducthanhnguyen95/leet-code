ee3b274155b67c3aed0a09d196a5077d42da50d8
thanhnd <ducthanhnguyen95@gmail.com>
2025-05-02 23:34:02 +0700

    Longest Common Prefix

---
+++ easy/14_longest_common_prefix/14_longest_common_prefix.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	test := []string{"flower", "flow", "flight"}
+	fmt.Println("Vertical:", longestCommonPrefixVertical(test))
+
+}
+
+// 1. Vertical Scanning
+func longestCommonPrefixVertical(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	for i := 0; i < len(strs[0]); i++ {
+		c := strs[0][i]
+		for j := 1; j < len(strs); j++ {
+			if i >= len(strs[j]) || strs[j][i] != c {
+				return strs[0][:i]
+			}
+		}
+	}
+	return strs[0]
+}
